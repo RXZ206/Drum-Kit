88ac17cf341bdfb4ca9ae05c7434c8a6cb07fcde
Ruixi <ruixizhang2006@gmail.com>
2020-10-30 20:33:26 -0400

    finish project

---
+++ index.js
@@ -0,0 +1,50 @@
+let drumButton_length = document.querySelectorAll(".drum").length;
+
+//Button press
+for (let i = 0; i < drumButton_length; i++) {
+    document
+        .querySelectorAll("button")
+        [i].addEventListener("click", function () {
+            let buttonInnerHTML = this.innerHTML;
+
+            makeSound(buttonInnerHTML);
+        });
+}
+
+//Keyboard press
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            let kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
+    }
+}
